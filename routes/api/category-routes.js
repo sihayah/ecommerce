b6f1bd53378e14eb23a60436a3fa9b2d36f358cd
Sihayah <sihaya_a@hotmail.com>
2022-01-07 04:00:17 -0500

    put request for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -69,6 +69,17 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(category => {
+      res.json(category)
+    })
+    .catch(err => {
+      console.log(err)
+    })
 });
 
 router.delete('/:id', (req, res) => {
